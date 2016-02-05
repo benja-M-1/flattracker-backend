3218d30ec9528ae279410c05fecf82e6033d3b14
Samy Ghribi <samy.ghribi@student.ecp.fr>
2016-02-05 14:48:00 +0100

    Update API with user createOrUpdate method

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -25,27 +25,52 @@ class UserController extends Controller
      *  resource=true,
      *  section="Users",
      *  output={"class"="AppBundle\Entity\User"},
-     *  description="Create user"
+     *  description="Create or update user"
      * )
      */
-    public function createUserAction(Request $request)
+    public function createOrUpdateUserAction(Request $request)
     {
         $user = new User();
         $form = $this->createForm(new UserType(), $user);
+        $em = $this->getDoctrine()->getManager();
 
         $form->handleRequest($request);
 
         if ($form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
             $view = View::create($user, Response::HTTP_CREATED);
         } else {
-            $view = View::create($form, Response::HTTP_BAD_REQUEST);
+            $existingUser = $em->getRepository('AppBundle:User')->findOneBy(array('facebookId' => $user->getFacebookId()));
+            if ($existingUser) {
+                $view = View::create($existingUser, Response::HTTP_OK);
+            } else {
+                $view = View::create($form, Response::HTTP_BAD_REQUEST);
+            }
         }
 
         $handler = $this->get('fos_rest.view_handler');
 
         return $handler->handle($view);
+    }
+
+    /**
+     * @TODO: not really REST compliant...
+     *
+     * @Rest\Get("/{id}/visits-as-searcher", defaults={"_format" = "json"})
+     */
+    public function getUserVisitsAsSearcherAction(User $user)
+    {
+        return $user->getAsSearcherVisits();
+    }
+
+    /**
+     * @TODO: not really REST compliant...
+     *
+     * @Rest\Get("/{id}/visits-as-tracker", defaults={"_format" = "json"})
+     */
+    public function getUserVisitsAsTrackerAction(User $user)
+    {
+        return $user->getAsSearcherVisits();
     }
 }
